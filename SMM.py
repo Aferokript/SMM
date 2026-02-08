ba3c0a34b3e90cc6f0eb7ad183fbcc411c6e3903
Aferokript <alberto.cardinale@bk.ru>
2026-02-08 21:19:53 +0300

    Update SMM.py

--- SMM.py
+++ SMM.py
@@ -2,74 +2,74 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-token = os.environ['TOKEN']
-
-
-def shorted_link(token, link):
+def cut_link(token, original_url):
     params = {
         'v': '5.199',
         'access_token': token,
-        'url': link,
+        'url': original_url,
         'private': 0
     }
-
-    try:
-        response = requests.get('https://api.vk.com/method/utils.getShortLink', params=params)
-        response.raise_for_status()
-        data = response.json()
-        if 'error' in data:
-            return 'Ошибка'
-        else:
-            return data['response']['short_url']
-    except requests.exceptions.HTTPError:
-        return 'Ошибка'
+    response = requests.get('https://api.vk.com/method/utils.getShortLink', params=params)
+    return response.json()
 
 
-def count_clicks(token, link):
-    if 'vk.cc/' not in link:
-        return 'Ошибка'
-
-    key = link.split('vk.cc/')[-1]
-
+def count_clicks(token, url_key):
     params = {
         'v': '5.199',
         'access_token': token,
-        'key': key,
+        'key': url_key,
         'interval': 'forever',
         'interval_counts': 'forever',
         'extended': 1
     }
-
-    try:
-        response = requests.get('https://api.vk.com/method/utils.getLinkStats', params=params)
-        response.raise_for_status()
-        data_click = response.json()
-        if 'error' in data_click:
-            return 'Ошибка'
-        else:
-            views = data_click["response"]["stats"][0]["views"]
-            return f'По ссылке перешло: {views}'
-    except requests.exceptions.HTTPError:
-        return 'Ошибка'
+    response = requests.get('https://api.vk.com/method/utils.getLinkStats', params=params)
+    return response.json()
 
 
-def is_shorted_link(link):
-    return 'vk.cc/' in link
+def is_shorted_link(token, url_to_check):
+    params = {
+        'v': '5.199',
+        'access_token': token,
+        'url': url_to_check,
+        'private': 0
+    }
+    response = requests.get('https://api.vk.com/method/utils.getShortLink', params=params)
+    short_url_data = response.json()
+    return 'error' not in short_url_data and 'vk.cc/' in url_to_check
 
 
 def main():
-    user_link = input('Введите ссылку ')
-    if is_shorted_link(user_link):
-        print(count_clicks(token, user_link))
-    else:
-        result = shorted_link(token, user_link)
-        if result != 'Ошибка':
-            print(f'Короткая ссылка: {result}')
+    try:
+        load_dotenv()
+        token = os.environ['VK_TOKEN']
+
+        user_link = input('Введите ссылку ')
+
+        if is_shorted_link(token, user_link):
+            url_key = user_link.split('vk.cc/')[-1]
+            link_stats_data = count_clicks(token, url_key)
+
+            if 'error' in link_stats_data:
+                print('Ошибка')
+            else:
+                views_count = link_stats_data["response"]["stats"][0]["views"]
+                print(f'По ссылке перешло: {views_count}')
         else:
-            print('Ошибка')
+            short_url_response = cut_link(token, user_link)
+
+            if 'error' in short_url_response:
+                print('Ошибка')
+            else:
+                short_url_value = short_url_response['response']['short_url']
+                print(f'Короткая ссылка: {short_url_value}')
+
+    except requests.exceptions.HTTPError:
+        print('Ошибка')
+    except KeyError:
+        print('Ошибка: VK_TOKEN не найден')
 
 
 if __name__ == '__main__':
     main()
+
